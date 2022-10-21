6d7b413e6cfd52725a850d2edf9b3ae6591c0fc6
johannes67890 <johannes@orager.dk>
2022-10-21 23:00:25 +0200

    convert to class

--- src/curve/keyGeneration.ts
+++ src/curve/keyGeneration.ts
@@ -1,5 +1,6 @@
-import { BN } from "bn.js";
+import BN from "bn.js";
 import { utils } from "hash.js";
+import { sha256 } from "hash.js";
 import { secp256k1, curveOpt } from "./curvesDefined";
 import { Buffer } from "buffer";
 import { randomBytes } from "crypto";
@@ -9,40 +10,55 @@ interface Key {
   y: string;
 }
 
-export function generatePrivateKey(size: number | undefined): Key {
-  let PRIVATE_KEY: Key;
-  if (new BN(size).isEven()) {
-    PRIVATE_KEY = {
-      x: utils.toHex(randomBytes(size / 2)),
-      y: utils.toHex(randomBytes(size / 2)),
-    };
+class Keys {
+  public Gx: BN;
+  public Gy: BN;
+  public p: BN;
 
-    console.log(generatePublicKey(secp256k1, PRIVATE_KEY));
-  } else if (size == undefined) {
-    PRIVATE_KEY = {
-      x: utils.toHex(randomBytes(32 / 2)),
-      y: utils.toHex(randomBytes(32 / 2)),
-    };
-  } else throw new Error("Invalid key Size on Private key");
-  return PRIVATE_KEY;
-}
+  constructor(curve: curveOpt) {
+    this.Gx = new BN(curve.g.x, "hex");
+    this.Gy = new BN(curve.g.y, "hex");
+    this.p = new BN(curve.p, "hex");
+  }
 
-export function generatePublicKey(curve: curveOpt, privateKey: Key): Key {
   /**
-   * TODO: this might be wrong
-   * Dubble check
+   * generate random 'size' or 32 byte size private Key
    */
-  const PriavteX = new BN(privateKey.x, "hex");
-  const PriavteY = new BN(privateKey.y, "hex");
+  public generatePrivateKey(size: number | undefined): Key {
+    let PRIVATE_KEY;
+    if (new BN(size).isEven()) {
+      PRIVATE_KEY = {
+        x: utils.toHex(randomBytes(size / 2)),
+        y: utils.toHex(randomBytes(size / 2)),
+      };
+    } else if (size == undefined) {
+      PRIVATE_KEY = {
+        x: utils.toHex(randomBytes(32 / 2)),
+        y: utils.toHex(randomBytes(32 / 2)),
+      };
+    } else throw new Error("Invalid key Size on Private key");
+    return PRIVATE_KEY;
+  }
 
-  const Gx = new BN(curve.g.x, "hex");
-  const Gy = new BN(curve.g.x, "hex");
-  const p = new BN(curve.p, "hex");
+  public generatePublicKey(privateKey: Key): Key {
+    /**
+     * TODO: this might be wrong
+     * Dubble check
+     */
+    const PriavteX = new BN(privateKey.x, "hex");
+    const PriavteY = new BN(privateKey.y, "hex");
 
-  const pubX = PriavteX.mul(Gx).mod(p);
-  const pubY = PriavteY.mul(Gy).mod(p);
+    const pubX = PriavteX.mul(this.Gx).mod(this.p);
+    const pubY = PriavteY.mul(this.Gy).mod(this.p);
 
-  console.log(pubX.byteLength() + pubY.byteLength());
+    console.log(pubX.byteLength() + pubY.byteLength());
 
-  return { x: pubX.toString("hex"), y: pubY.toString("hex") };
+    return { x: pubX.toString("hex"), y: pubY.toString("hex") };
+  }
+
+  public sign(hashedMsg: string, privateKey: Key) {
+    //Method used: https://learnmeabitcoin.com/technical/ecdsa#elliptic-curves
+
+    const k = new BN(randomBytes(4), 16); // random number
+  }
 }
--- src/util.ts
+++ src/util.ts
@@ -4,3 +4,6 @@ import { utils } from "hash.js";
 export function parseBytes(bytes: string) {
   return utils.toArray(bytes, "hex");
 }
+export function hashMsgSHA256(message: string): string {
+  return hash.sha256().update(message).digest("hex");
+}
