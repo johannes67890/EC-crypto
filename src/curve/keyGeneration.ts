7e19f076f954d17d72248eab7c0ad805961a0ea7
johannes67890 <johannes@orager.dk>
2022-10-21 17:31:37 +0200

    pub & pri key generators

--- src/curve/keyGeneration.ts
+++ src/curve/keyGeneration.ts
@@ -4,36 +4,45 @@ import { secp256k1, curveOpt } from "./curvesDefined";
 import { Buffer } from "buffer";
 import { randomBytes } from "crypto";
 
-export function generatePrivateKey(size: number): Buffer {
-  const PRIVATE_KEY = Buffer.alloc(size, randomBytes(size));
-  console.log(generatePublicKey(secp256k1, PRIVATE_KEY));
+interface Key {
+  x: string;
+  y: string;
+}
 
+export function generatePrivateKey(size: number | undefined): Key {
+  let PRIVATE_KEY: Key;
+  if (new BN(size).isEven()) {
+    PRIVATE_KEY = {
+      x: utils.toHex(randomBytes(size / 2)),
+      y: utils.toHex(randomBytes(size / 2)),
+    };
+
+    console.log(generatePublicKey(secp256k1, PRIVATE_KEY));
+  } else if (size == undefined) {
+    PRIVATE_KEY = {
+      x: utils.toHex(randomBytes(32 / 2)),
+      y: utils.toHex(randomBytes(32 / 2)),
+    };
+  } else throw new Error("Invalid key Size on Private key");
   return PRIVATE_KEY;
 }
 
-export function generatePublicKey(curve: curveOpt, privateKey: Buffer) {
-  //console.log("Private", utils.toHex(Buffer.from(privateKey)));
+export function generatePublicKey(curve: curveOpt, privateKey: Key): Key {
+  /**
+   * TODO: this might be wrong
+   * Dubble check
+   */
+  const PriavteX = new BN(privateKey.x, "hex");
+  const PriavteY = new BN(privateKey.y, "hex");
 
-  //let pri = utils.toArray(Buffer.from(privateKey), "hex");
-  let pri = utils.toArray(
-    "46 b9 e8 61 b6 3d 35 09 c8 8b 78 17 27 5a 30 d2",
-    "hex"
-  );
-  let x = utils.toArray(curve.g.x, "hex");
-  let y = utils.toArray(curve.g.y, "hex");
-  let p = utils.toArray(curve.p, "hex");
-  let tx = [];
-  //let ty = [];
-  for (var i = 0; i < 32; i++) {
-    tx[i] = x[i] * pri[i];
-    // for (let j = 0; j < 32; j++) {
-    //   ty[j] = y[j] * pri[j];
-    // }
-  }
-  console.log("pri", utils.toHex(pri));
+  const Gx = new BN(curve.g.x, "hex");
+  const Gy = new BN(curve.g.x, "hex");
+  const p = new BN(curve.p, "hex");
 
-  console.log("x", utils.toHex(tx));
-  // console.log("y", utils.toHex(ty).length);
+  const pubX = PriavteX.mul(Gx).mod(p);
+  const pubY = PriavteY.mul(Gy).mod(p);
 
-  return Buffer.alloc(64);
+  console.log(pubX.byteLength() + pubY.byteLength());
+
+  return { x: pubX.toString("hex"), y: pubY.toString("hex") };
 }
