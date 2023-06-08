bc4b7f6c7ab14b6043d0cc926c0d6e6109c920bd
Johannes <johannes@orager.dk>
2023-06-08 20:50:26 +0200

    setup flowbite

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import { useState } from 'react'
-import KeySet from './ECC/EcDSA/keyGeneration'
-
-import { secp256k1 } from './ECC/curvesDefined'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-const keyset = new KeySet(secp256k1)
-
-  console.log(keyset.privateKey.toString('hex'));
   
   return (
     <>
-      <div className='w-10 bg-red-100 h-20'>
-        <button onClick={() => setCount(count + 1)}>Click me</button>
-      </div>
     
     </>
   )
